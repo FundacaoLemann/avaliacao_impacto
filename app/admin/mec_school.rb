6d1fed8087dff74d95c4a60b96726b6c54874b74
fladson <fladsonthiago@gmail.com>
2017-10-04 17:54:58 -0300

    Improve ActiveAdmin layout

--- app/admin/mec_school.rb
+++ app/admin/mec_school.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register MecSchool do
+  menu label: "Escola MEC"
   permit_params :inep, :name
-  actions :all, except: [:new, :edit, :destroy]
+  actions :index
+  remove_filter :created_at, :updated_at
+
+  index do
+    column :inep
+    column :name
+  end
 end
