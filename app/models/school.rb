4a6880857901081bc8697a45945828967aa7ae54
fladson <fladsonthiago@gmail.com>
2017-11-01 13:36:00 -0300

    Remove 'privada' from school administrations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,6 @@
 class School < ApplicationRecord
   validates :inep, uniqueness: true
-  ADMINISTRATIONS = %w[Privada Federal Estadual Municipal].freeze
+  ADMINISTRATIONS = %w[Federal Estadual Municipal].freeze
 
   def to_s
     "#{inep} - #{name}"
