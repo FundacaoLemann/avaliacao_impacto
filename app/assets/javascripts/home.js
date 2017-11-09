245b5ff62cdeed90b2596e74e3e43dd939b00f74
Fernando Paiva <fernando@fundacaolemann.org.br>
2017-11-09 18:36:53 -0200

    Correcting comments

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,7 +10,7 @@ $(function() {
       method: 'GET'
     });
     $('#city').removeAttr('disabled');
-    if(state == 7){ // Distrito Federal has only one city and avoids 
+    if(state == 7){ // Distrito Federal has only one city and on change trigger doesnt work  
         $('#administration').removeAttr('disabled');
     }
   });
