9e239ec51e3a8cda26719475f9d2191b99de94d4
fladson <fladsonthiago@gmail.com>
2017-11-14 14:19:07 -0300

    Fix getStateUrl

--- app/assets/javascripts/administration.js
+++ app/assets/javascripts/administration.js
@@ -33,12 +33,12 @@ function getFormOption(adm){
 // get state url based on the current route
 function getStateUrl(){
   var stateUrl = '/admin/form_options.json?' +
-                 'q%5Bform_name%5D=baseline' +
+                 'q%5Bform_name_eq%5D=baseline' +
                  '&q%5Bstate_or_city_equals%5D=' + stateName +
                  '&q%5Bdependencia_desc_equals%5D=Estadual';
   if ($(".home.follow_up").length > 0) {
     stateUrl = '/admin/form_options.json?' +
-               'q%5Bform_name%5D=follow_up' +
+               'q%5Bform_name_eq%5D=follow_up' +
                '&q%5Bstate_or_city_equals%5D=' + stateName +
                '&q%5Bdependencia_desc_equals%5D=Estadual';
   }
