8591c8a37ba915dd68a05e8bfa401bf04a746c2c
fladson <fladsonthiago@gmail.com>
2017-11-27 14:14:09 -0300

    Add administrations allowed condition on baseline

--- app/assets/javascripts/administration.js
+++ app/assets/javascripts/administration.js
@@ -10,12 +10,42 @@ $(function() {
     cityName = $("#city option:selected").text();
     city = $("#city").val();
     administration = $("#administration").val();
-    $('#school').removeAttr('disabled');
+    checkAllowedAdministrations();
     deadline = "";
     faParams = "";
-    getFormOption(administration);
   });
 });
+
+function checkAllowedAdministrations() {
+  if (($(".home.index").length > 0) && !isAdministrationAllowed()) {
+    swal({
+      title: 'Olá, você está no link errado',
+      text: 'Não se preocupe. Para ler as instruções corretas e preencher o questionário da sua rede por favor clique no link abaixo.',
+      content: {
+        element: "a",
+        attributes: {
+          href: 'http://questionarioformar.fundacaolemann.org.br/fup6271',
+          innerHTML: 'Ir para o questionário correto',
+        }
+      },
+      icon: 'warning',
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+      button: false
+    })
+  }else {
+    $('#school').removeAttr('disabled');
+    getFormOption(administration);
+  }
+}
+
+function isAdministrationAllowed(){
+  // 3516309 - Francisco Morato | 2111300 - São Luís | 1100205 - Porto Velho
+  if((city == '3516309' || city == '2111300' || city == '1100205') && administration == 'Municipal'){
+    return true;
+  }
+  return false;
+}
 
 // get form assembly params and deadline from admin route
 function getFormOption(adm){
