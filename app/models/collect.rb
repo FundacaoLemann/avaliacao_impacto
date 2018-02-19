7e0d7caf1fef5b32d2d2cefaad4c2488c9600c49
Fladson Gomes <fladsonthiago@gmail.com>
2018-02-19 11:47:32 -0300

    Add active_admin page for collect

--- app/models/collect.rb
+++ app/models/collect.rb
@@ -1,2 +1,24 @@
 class Collect < ApplicationRecord
+  attr_accessor :administrations_raw
+
+  def administrations_raw=(items)
+    self.administrations = items.split("\r\n")
+  end
+
+  def administrations_raw
+    administrations.join("\n")
+  end
+
+  def form_sections=(items)
+    items.delete('')
+    super items
+  end
+
+  def parsed_form_sections
+    form_sections.join(" ")
+  end
+
+  def parsed_administrations
+    administrations.join("<br>")
+  end
 end
