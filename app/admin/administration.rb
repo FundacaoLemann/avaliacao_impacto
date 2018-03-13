f3e6cf89233117c132158d4687bb0c8e62014372
Fladson Gomes <fladsonthiago@gmail.com>
2018-03-13 14:58:08 -0300

    Add contact_name to Administration ActiveAdmin page

--- app/admin/administration.rb
+++ app/admin/administration.rb
@@ -1,17 +1,20 @@
 ActiveAdmin.register Administration do
   menu priority: 8, if: -> { current_admin_user.sub_admin? }
-  permit_params :adm, :state_id, :city_id, :preposition, :name, :cod
-  actions :all, except: [:edit, :show]
+  permit_params :adm, :state_id, :city_id, :preposition, :name, :cod, :contact_name
+  actions :all, except: [:show]
   config.batch_actions = false
   breadcrumb do
   end
 
   filter :name_cont, label: "Descrição"
   filter :cod_cont, label: "Código da Rede"
+  filter :contact_name_cont, label: "Responsável"
   filter :adm, label: "", as: :check_boxes, collection: Administration.adms.collect { |k,v| [k, v]}
+
   index do
     column :name
     column :cod
+    column :contact_name
     actions
   end
 
@@ -22,6 +25,7 @@ ActiveAdmin.register Administration do
       input :state_id, as: :select, collection: State.all.order(:name), input_html: { disabled: true }
       input :city_id, as: :select, collection: City.first(2), input_html: { disabled: true }
       input :preposition, as: :select, collection: %w[da de do], input_html: { disabled: true }
+      input :contact_name
       input :name, input_html: { readonly: true }
       input :cod, input_html: { readonly: true }
     end
