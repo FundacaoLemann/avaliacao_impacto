41f27d7b8b22fd0a743d0c0298df7c25656d12d2
Fladson Gomes <fladsonthiago@gmail.com>
2018-03-14 10:38:10 -0300

    Add contact_name to Administrations (#56)
    * Add contact_name to Administrations
    
    * Add contact_name to Administration ActiveAdmin page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312133711) do
+ActiveRecord::Schema.define(version: 20180313175254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180312133711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cod"
+    t.string "contact_name"
     t.index ["adm"], name: "index_administrations_on_adm"
     t.index ["city_id"], name: "index_administrations_on_city_id"
     t.index ["cod"], name: "index_administrations_on_cod"
