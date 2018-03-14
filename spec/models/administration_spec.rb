ab996be460774a2e764b595bca77c7c5aa08ad85
Fladson Gomes <fladsonthiago@gmail.com>
2018-03-14 09:08:45 -0300

    Fix specs

--- spec/models/administration_spec.rb
+++ spec/models/administration_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Administration, type: :model do
-  it { is_expected.to belong_to(:state) }
-  it { is_expected.to belong_to(:city) }
   it { is_expected.to have_and_belong_to_many(:collects) }
   it { is_expected.to validate_uniqueness_of(:name) }
 
