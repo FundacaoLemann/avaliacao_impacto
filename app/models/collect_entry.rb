76fa5557e787cdbf49453544b0b89a673962da19
Fladson Gomes <fladsonthiago@gmail.com>
2018-04-11 13:26:27 -0300

    Adjust identation on CollectEntry

--- app/models/collect_entry.rb
+++ app/models/collect_entry.rb
@@ -1,5 +1,5 @@
 class CollectEntry < ApplicationRecord
   belongs_to :collect
-  belongs_to :administration, foreign_key: 'adm_cod', primary_key: 'cod',  optional: true
-  belongs_to :school, foreign_key: 'school_inep', primary_key: 'inep',  optional: true
+  belongs_to :administration, foreign_key: 'adm_cod', primary_key: 'cod', optional: true
+  belongs_to :school, foreign_key: 'school_inep', primary_key: 'inep', optional: true
 end
