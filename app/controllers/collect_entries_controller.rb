5ae0f3dfc639843e7b21ebfd841387c00ce38590
Fladson Gomes <fladsonthiago@gmail.com>
2018-04-20 15:54:08 -0300

    Add quitter route to collect entries

--- app/controllers/collect_entries_controller.rb
+++ app/controllers/collect_entries_controller.rb
@@ -3,7 +3,12 @@ class CollectEntriesController < ApplicationController
   before_action :set_collect_entry, only: :update
 
   def update
-    @collect_entry.update(substitute: collect_entry_params[:substitute]) if @collect_entry
+    @collect_entry&.update(collect_entry_params)
+    # this is a quick fix because I could not filter the in progress label on pipefy
+    if collect_entry_params[:quitter]
+      submission = Submission.where(card_id: collect_entry_params[:card_id])
+      submission&.update(status: :quitter)
+    end
 
     head :ok
   end
@@ -16,6 +21,6 @@ class CollectEntriesController < ApplicationController
   end
 
   def collect_entry_params
-    params.require(:collect_entry).permit(:card_id, :substitute)
+    params.require(:collect_entry).permit(:card_id, :substitute, :quitter)
   end
 end
