64ab1f85ca6b92a0fca63cf76f95c616ca1a00f5
Fladson Gomes <fladsonthiago@gmail.com>
2018-05-08 15:03:54 -0300

    Fix ReplaceQuitterWorker NoMethodError

--- app/workers/replace_quitter_worker.rb
+++ app/workers/replace_quitter_worker.rb
@@ -4,7 +4,11 @@ class ReplaceQuitterWorker
   def perform(collect_entry_id)
     collect_entry = CollectEntry.find(collect_entry_id)
     collect = collect_entry.collect
-    substitute = collect.where(administration: collect_entry.administration, group: "Repescagem").first
+    substitute = CollectEntry.where(
+      collect: collect,
+      administration: collect_entry.administration,
+      group: "Repescagem"
+    ).first
     substitute.update(substitute: true)
     pipe = collect.pipe
     PipefyApi.post(pipe.move_card_to_phase(substitute.card_id, :triagem))
