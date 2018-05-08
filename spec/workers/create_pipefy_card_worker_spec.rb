eac55989bd166b3417cd9ceaed040898675f6b19
Fladson Gomes <fladsonthiago@gmail.com>
2018-05-08 11:22:21 -0300

    Update worker specs namespace

--- spec/workers/create_pipefy_card_worker_spec.rb
+++ spec/workers/create_pipefy_card_worker_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
-RSpec.describe CreatePipefyCardsWorker, type: :worker do
+RSpec.describe CreatePipefyCardWorker, type: :worker do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/workers/replace_quitter_worker_spec.rb
+++ spec/workers/replace_quitter_worker_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
-RSpec.describe SubstituteQuitterWorkerWorker, type: :worker do
+RSpec.describe ReplaceQuitterWorker, type: :worker do
   pending "add some examples to (or delete) #{__FILE__}"
 end
