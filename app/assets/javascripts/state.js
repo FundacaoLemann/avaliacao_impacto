4fa3724099077a1d8f8e8c8ff5037ab435da4481
Fladson Gomes <fladsonthiago@gmail.com>
2018-05-14 09:47:40 -0300

    Require home inputs

--- app/assets/javascripts/state.js
+++ app/assets/javascripts/state.js
@@ -3,12 +3,32 @@ var stateName = "";
 
 $(function() {
   $("#state").on('change', function() {
+    checkRequiredInputs();
     state = parseInt($("#state").val());
     stateName = $("#state option:selected").text();
     $.ajax({
       url: '/state/' + state,
       method: 'GET'
     });
-    $('#city').removeAttr('disabled');
   });
 });
+
+function checkRequiredInputs() {
+  name = $("#name").val();
+  email = $("#email").val();
+  personal_phone = $("#personal_phone").val();
+  phone = $("#phone").val();
+  if (isEmpty(name) || isEmpty(email) || isEmpty(personal_phone) || isEmpty(phone)) {
+    swal({
+      title: 'Informações obrigatórias',
+      text: 'Antes de prosseguir, por favor preencha as informações de contato acima.',
+      icon: 'warning',
+    })
+  }else{
+    $('#city').removeAttr('disabled');
+  }
+}
+
+function isEmpty(str){
+    return !str.replace(/^\s+/g, '').length;
+}
