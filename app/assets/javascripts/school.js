439b75afa8e1d494ed89a4d6e01f88b52c715a36
Jessika Darambaris <jessika.darambaris@gmail.com>
2018-10-11 16:23:29 -0300

    search schools with select2 - stable version

--- app/assets/javascripts/school.js
+++ app/assets/javascripts/school.js
@@ -1,6 +1,6 @@
 // school search
 document.addEventListener("turbolinks:load", function() {
-  $input = $("[data-behavior='search']");
+ /* $input = $("[data-behavior='search']");
   var options = {
     adjustWidth: false,
     getValue: 'name',
@@ -16,7 +16,64 @@ document.addEventListener("turbolinks:load", function() {
         $("#school_id").val(schoolId).trigger("change");
       }
     }
-  };
+  }; */
 
-  $input.easyAutocomplete(options);
+  /*var options2 = {
+   // getValue: 'name',
+    url: function(phrase) {
+      return ('/search.json?city=' + city + '&administration=' + administration
+              + '&collect_id=' + collectId + '&=' + currentAdm.cod + '&school=' + phrase);
+    }
+  };*/
+  //$input.easyAutocomplete(options);
+ 
+
+  //var json_schools = {"schools": [{"id": 1, "text": "Escola 1"}, {"id": 2, "text": "Escola 2"}]};
+
+  /*var select2_var = {
+    url: '/search.json',
+    delay: 250,
+    method: 'GET',
+    data: function (params, phrase) {
+      var query = {
+        search: params.term,
+        city: city,
+        administration: administration,
+        collect_id: collectId,
+        adm_cod: currentAdm.cod,
+        school: phrase,
+      }
+      return query;
+    },
+    /*processResults: function (data, params) {
+      return {
+        results: $.map(data['schools'], function(value, index){
+          return {id: value.id, text: value.name};
+        })
+      };
+    } */ 
+//}; 
+
+  $("#school").select2({
+    ajax: {
+      url: function(params) {
+        var school = (!params.term)?'':params.term;      
+        return ('/search.json?city=' + city + '&administration=' + administration
+                + '&collect_id=' + collectId + '&=' + currentAdm.cod + '&school='+school);
+      },
+      dataType: 'json',
+      data: function (params) {
+          search: params.term;
+      },
+      processResults: function (data) {
+        return {
+          results: $.map(data["schools"], function(value, index){
+            return {id: value.school_id, text: value.school_id + ' | ' + value.name};
+          })
+        };
+      },
+      cache: true
+    }
+  });
+
 });
