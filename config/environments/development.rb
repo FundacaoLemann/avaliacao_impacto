215c4403f4d925fc8e1246b5f20cc454f9ac3ca4
Laury Bueno <laury@hacklab.com.br>
2019-04-16 11:05:47 -0300

    [Dev] Allow the console to be called through Docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # In development mode, allow any IP to use the console
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
