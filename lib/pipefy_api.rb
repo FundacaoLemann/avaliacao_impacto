614bff5c927eb5d807319cb85b0424b02b93c552
Laury Bueno <laury@hacklab.com.br>
2019-05-03 13:49:54 -0300

    [Logging] Since RestClient may raise an exception, prepare debug data beforehand

--- lib/pipefy_api.rb
+++ lib/pipefy_api.rb
@@ -6,12 +6,18 @@ module PipefyApi
       authorization: "Bearer #{ENV['PIPEFY_TOKEN']}"
     }
 
+    # Since RestClient may raise an exception, prepare some debug data beforehand
+    Raven.capture_message("Error interacting with Pipefy API", :extra => {'request_body'=> values.to_json})
+
     response = RestClient.post(endpoint, values, headers)
 
     if response.code != 200 && response.code != 201
       # Some error may have ocurred; report this to Sentry now
       Raven.capture_message("Error interacting with Pipefy API", :extra => {'request_body'=> values.to_json, 'response_code' => response.code, 'response' => response.to_json})
     end
+
+    # Clear the Raven context now, so future errors won't be reported with this data
+    Raven::Context.clear!
 
     return response
 
